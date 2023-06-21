a0c347520eb206ef71eb4d407c7781020fb2f99e
Chris Pinkney <hey@chrispinkney.com>
2023-06-21 19:44:42 -0400

    chore: ui skeleton

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let users: User[] = [];
+
+	onMount(async () => {
+		const response = await fetch('http://localhost:7000');
+		users = await response.json();
+	});
+</script>
+
+<h1>Users</h1>
+
+<ul>
+	{#each users as user}
+		<li>{user.email}</li>
+	{/each}
+</ul>
