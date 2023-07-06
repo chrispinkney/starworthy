f279796b6ffef2e3b5e83ba9c900e1ab91d2b1ef
Chris Pinkney <hey@chrispinkney.com>
2023-07-06 15:17:19 -0400

    feat(web): implement banner component and logo

---
+++ web/src/lib/components/Banner/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<header>
+	<div class="max-w-screen-xl mx-auto py-4 px-8">
+		<img src="/starworthy.svg" alt="site logo" class="logo mx-auto max-w-full h-auto" />
+	</div>
+</header>
+
+<style>
+	.logo {
+		filter: invert(50%) sepia(24%) saturate(7165%) hue-rotate(339deg) brightness(97%) contrast(99%);
+	}
+</style>
